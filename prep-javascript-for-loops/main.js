d021e35063ac9ede0a34dd806c4cd0813696ca08
Henry Nichols <nicholsh562@gmail.com>
2021-03-04 12:42:54 -0800

    Completed prep-javascript-for-loops

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,15 @@
+// create your loops here.
+var number = 10
+for (var i = 0; i < number; i++) {
+  console.log(i)
+}
+
+  var number = 20
+  for (var i = 0; i < number; i += 2) {
+    console.log(i)
+  }
+
+var number = 0
+for (var i = 100; i > number; i--) {
+  console.log('Time till explosion!!',i)
+}
