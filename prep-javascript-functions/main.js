ac51ac9bf6d98b3495598798e3a7614bb7f80763
Henry Nichols <nicholsh562@gmail.com>
2021-03-04 11:11:19 -0800

    Completed prep-javascript-functions

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -16,6 +16,12 @@ function multiplyAndDivideBy5(num1, num2){
 function subtractTwoNumbers(num1, num2){
   return (num1 - num2)
 }
+
+function getCircleCircumference(radius){
+  return ( 3.14 * 2) * radius
+}
+
+
 function getFullName(firstName, lastName){
   return (firstName + " " +lastName)
 }
@@ -24,13 +30,12 @@ function cube(number){
 }
 
 
-
-
 console.log(addTwoNumbers(4, 4));
 console.log(convertHoursToMinutes(2))
 console.log(personalizeGreeting('World!'))
 console.log(addAndMultiplyBy5(10, 5))
 console.log(multiplyAndDivideBy5(35, 10))
 console.log(subtractTwoNumbers(22,7))
+console.log(getCircleCircumference(5))
 console.log(getFullName("Juan", "Ramirez"))
 console.log(cube(5))
